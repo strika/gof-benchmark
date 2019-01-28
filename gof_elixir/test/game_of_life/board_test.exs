4c6d72e5a47c008de5f45e828a7556bf193fcf34
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-01-28 20:01:49 +0000

    Add failing spec

--- gof_elixir/test/game_of_life/board_test.exs
+++ gof_elixir/test/game_of_life/board_test.exs
@@ -8,25 +8,26 @@ defmodule GameOfLife.BoardTest do
 
   describe ".parse/2" do
     test "parses the board into world", %{world: world} do
-      board = "0,1,0\n1,0,1\n0,1,0"
+      board = "0,1,0\n1,0,0\n1,1,0"
 
       GameOfLife.Board.parse(board, world)
 
       assert GameOfLife.World.state(world) == %{
-        0 => %{ 0 => 0, 1 => 1, 2 => 0 },
+        0 => %{ 0 => 0, 1 => 1, 2 => 1 },
         1 => %{ 0 => 1, 1 => 0, 2 => 1 },
-        2 => %{ 0 => 0, 1 => 1, 2 => 0 }
+        2 => %{ 0 => 0, 1 => 0, 2 => 0 }
       }
     end
   end
 
   describe ".generate/1" do
     test "generates string from worlds state", %{world: world} do
+      GameOfLife.World.set(world, 0, 0, 1)
       GameOfLife.World.set(world, 1, 0, 1)
       GameOfLife.World.set(world, 1, 1, 1)
       GameOfLife.World.set(world, 1, 2, 1)
 
-      assert GameOfLife.Board.generate(world) == "0,1,0\n0,1,0\n0,1,0"
+      assert GameOfLife.Board.generate(world) == "1,1,0\n0,1,0\n0,1,0"
     end
   end
 end
