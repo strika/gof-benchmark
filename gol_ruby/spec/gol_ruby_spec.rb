f9546a6f37afb500c7eadbc9e2025c2ed99436de
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-02-20 20:10:21 +0000

    Remove pending specs

--- gol_ruby/spec/gol_ruby_spec.rb
+++ gol_ruby/spec/gol_ruby_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe GolRuby do
-  it "has a version number" do
-    expect(GolRuby::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
