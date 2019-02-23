07f3435b53b5ececac9ab3428fe8bd4c09ae52db
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-02-23 11:06:56 +0000

    Implement Cell#next_state

--- gol_ruby/lib/cell.rb
+++ gol_ruby/lib/cell.rb
@@ -15,6 +15,18 @@ class Cell
     @alive = true
   end
 
+  def next_state(alive_in_neighborhood)
+    if alive?
+      if alive_in_neighborhood < 2 || alive_in_neighborhood > 3
+        kill
+      end
+    else
+      if alive_in_neighborhood == 3
+        revive
+      end
+    end
+  end
+
   def to_s
     alive? ? "1" : "0"
   end
--- gol_ruby/spec/cell_spec.rb
+++ gol_ruby/spec/cell_spec.rb
@@ -50,4 +50,67 @@ RSpec.describe Cell do
       end
     end
   end
+
+  describe "#next_state" do
+    context "when cell is alive" do
+      before do
+        @cell = Cell.new(:alive => true)
+      end
+
+      context "when the cell has fewer than 2 alive neighbours" do
+        it "dies" do
+          @cell.next_state(1)
+          expect(@cell.alive?).to be_falsy
+        end
+      end
+
+      context "when the cell has 2 alive neighbours" do
+        it "stays alive" do
+          @cell.next_state(2)
+          expect(@cell.alive?).to be_truthy
+        end
+      end
+
+      context "when the cell has 3 alive neighbours" do
+        it "stays alive" do
+          @cell.next_state(3)
+          expect(@cell.alive?).to be_truthy
+        end
+      end
+
+      context "when the cell has more than 3 alive neighbours" do
+        it "dies" do
+          @cell.next_state(4)
+          expect(@cell.alive?).to be_falsy
+        end
+      end
+    end
+
+    context "when cell is dead" do
+      before do
+        @cell = Cell.new
+      end
+
+      context "when the cell has less than 3 alive neighbours" do
+        it "stays dead" do
+          @cell.next_state(2)
+          expect(@cell.alive?).to be_falsy
+        end
+      end
+
+      context "when the cell has 3 alive neighbours" do
+        it "is revived" do
+          @cell.next_state(3)
+          expect(@cell.alive?).to be_truthy
+        end
+      end
+
+      context "when the cell has more than 3 alive neighbours" do
+        it "stays dead" do
+          @cell.next_state(4)
+          expect(@cell.alive?).to be_falsy
+        end
+      end
+    end
+  end
 end
