923f49b0537ec00c29b8a4b84fcd882e17a50cd5
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-02-23 10:54:52 +0000

    Implement Board#alive_in_neighborhood

--- gol_ruby/lib/board.rb
+++ gol_ruby/lib/board.rb
@@ -3,13 +3,33 @@ require "cell"
 class Board
   attr_reader :size
 
+  def self.neighborhood_coordinates(x, y)
+    neighborhood_with_center = (-1..1).flat_map do |delta_x|
+      (-1..1).map do |delta_y|
+        [x - delta_x, y - delta_y]
+      end
+    end
+
+    neighborhood_with_center.delete([x, y])
+    neighborhood_with_center
+  end
+
   def initialize(size)
     @size = size
     @board = build_board
   end
 
   def get(x, y)
+    return Cell.new if out_of_bounds?(x, y)
+
     @board[y][x]
+  end
+
+  def alive_in_neighbourhood(x, y)
+    self.class.neighborhood_coordinates(x, y)
+        .map { |coordinates| get(*coordinates) }
+        .select(&:alive?)
+        .count
   end
 
   def to_s
@@ -22,5 +42,9 @@ class Board
     Array.new(@size) do
       Array.new(@size) { Cell.new }
     end
+  end
+
+  def out_of_bounds?(x, y)
+    x.negative? || y.negative? || x >= size || y >= size
   end
 end
--- gol_ruby/spec/board_spec.rb
+++ gol_ruby/spec/board_spec.rb
@@ -1,6 +1,14 @@
 require "board"
 
 RSpec.describe Board do
+  describe ".neighborhood_coordinates" do
+    it "returns coordinate of cells from neighborhood" do
+      expect(Board.neighborhood_coordinates(1, 1)).to match_array([
+        [0, 0], [1, 0], [2, 0], [0, 1], [2, 1], [0, 2], [1, 2], [2, 2]
+      ])
+    end
+  end
+
   describe "#initialize" do
     it "creates a board with dead cells" do
       board = Board.new(4)
@@ -12,6 +20,39 @@ RSpec.describe Board do
     end
   end
 
+  describe "#get" do
+    context "when coordinates are out of bounds" do
+      before do
+        @board = Board.new(1)
+        @board.get(0, 0).revive
+      end
+
+      context "when x is lower than 0" do
+        it "returns a dead cell" do
+          expect(@board.get(-1, 0).alive?).to be_falsy
+        end
+      end
+
+      context "when y is lower than 0" do
+        it "returns a dead cell" do
+          expect(@board.get(0, -1).alive?).to be_falsy
+        end
+      end
+
+      context "when x is higher than or equal to size" do
+        it "returns a dead cell" do
+          expect(@board.get(1, 0).alive?).to be_falsy
+        end
+      end
+
+      context "when y is higher than or equal to size" do
+        it "returns a dead cell" do
+          expect(@board.get(0, 1).alive?).to be_falsy
+        end
+      end
+    end
+  end
+
   describe "#to_s" do
     it "returns String representation of the board" do
       board = Board.new(2)
@@ -20,6 +61,31 @@ RSpec.describe Board do
       board.get(1, 0).revive
 
       expect(board.to_s).to eq("1,1\n0,0")
+    end
+  end
+
+  describe "#alive_in_neighbourhood" do
+    # Board:
+    #
+    # 1 1 1
+    # 1 0 0
+    # 0 0 1
+
+    before do
+      @board = Board.new(3)
+      @board.get(0, 0).revive
+      @board.get(1, 0).revive
+      @board.get(2, 0).revive
+      @board.get(0, 1).revive
+      @board.get(2, 2).revive
+    end
+
+    it "returns the number of alive cells in neighbourhood" do
+      expect(@board.alive_in_neighbourhood(0, 0)).to eq(2)
+      expect(@board.alive_in_neighbourhood(1, 0)).to eq(3)
+      expect(@board.alive_in_neighbourhood(2, 0)).to eq(1)
+      expect(@board.alive_in_neighbourhood(0, 1)).to eq(2)
+      expect(@board.alive_in_neighbourhood(2, 2)).to eq(0)
     end
   end
 end
