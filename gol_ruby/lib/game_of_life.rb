abecf3f7c8cf07919f497b37cde9b711861276bd
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-02-23 12:12:35 +0000

    Implement benchmarking

--- gol_ruby/lib/game_of_life.rb
+++ gol_ruby/lib/game_of_life.rb
@@ -1,8 +1,15 @@
+require "benchmark"
 require "board"
 
 class GameOfLife
   def run
     board = parse_board
+
+    time = Benchmark.realtime do
+      1000.times { board.next_state }
+    end
+
+    puts "Completed in #{time * 1000}ms"
 
     File.write("result.csv", board)
   end
